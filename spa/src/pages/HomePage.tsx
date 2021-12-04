7e01e29479bbe6d76f2f9c1defc76f0f4a38cb48
Edi Olteanu <ediolteanu@users.noreply.github.com>
2021-12-04 15:16:54 +0200

    added logo to homepage

--- spa/src/pages/HomePage.tsx
+++ spa/src/pages/HomePage.tsx
@@ -1,13 +1,19 @@
 import * as React from "react";
 
+import "../csss/HomePage.css";
+import logo from '../logo_softlink.png';
 interface Props {
 }
 
 const HomePage: React.FC<Props> = () => {
     return (
         <div>
-            
+            <div className="container ctr">
+                <img className="logo" src={logo} alt="Logo" />
+            </div>
+
         </div>
+       
     );
 }
 
