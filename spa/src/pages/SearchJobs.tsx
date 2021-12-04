351071bb17cfe25a0f45139e96d92445605f45ca
Edi Olteanu <ediolteanu@users.noreply.github.com>
2021-12-04 17:30:07 +0200

    Implemented home page

--- spa/src/pages/SearchJobs.tsx
+++ spa/src/pages/SearchJobs.tsx
@@ -6,23 +6,7 @@ interface Props {
 const SearchJobs: React.FC<Props> = () => {
     return (
         <div>
-            <div >Stack Exchange Search</div>
-            <div>
-                <p>This will search Stack Exchange questions for given title text.</p>
-                <p>
-                    Enter your search text and click the icon or press the Enter key. A
-                    list of matching questions will be shown.
-                </p>
-                <p>After that, you can click on a question to see the answers.</p>
-                <p>
-                    Full source available at{" "}
-                    <a href="https://github.com/reactjsexample/react-typescript-example">
-                        https://github.com/reactjsexample/react-typescript-example
-                    </a>
-                </p>
-                <h3>Written in React 16.9.0 with TypeScript</h3>
-                <h4>By JC Lango</h4>
-            </div>
+            This is SearchJobs.
         </div>
     );
 }
