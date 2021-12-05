6d2b2d1c74fba2b5306f2d167c451b983d13c4b9
Edi Olteanu <ediolteanu@users.noreply.github.com>
2021-12-05 02:12:31 +0200

    kinda finished frontend styling

---
+++ spa/src/components/FancyTable.tsx
@@ -0,0 +1,226 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
+import TableSortLabel from '@mui/material/TableSortLabel';
+import Paper from '@mui/material/Paper';
+import { visuallyHidden } from '@mui/utils';
+
+interface Data {
+  name: string;
+  link: string;
+}
+
+function createData(
+  name: string,
+  link: string,
+): Data {
+  return {
+    name,
+    link
+  };
+}
+
+const rows = [
+  createData('Cupcake', "305, 3.7, 67, 4.3"),
+  createData('Donut', "452, 25.0, 51, 4.9"),
+  createData('Eclair', "262, 16.0, 24, 6.0"),
+  createData('Frozen yoghurt', "159, 6.0, 24, 4.0"),
+  createData('Gingerbread', "356, 16.0, 49, 3.9"),
+  createData('Honeycomb', "408, 3.2, 87, 6.5"),
+  createData('Ice cream sandwich', "237, 9.0, 37, 4.3"),
+  createData('Jelly Bean', "375, 0.0, 94, 0.0"),
+  createData('KitKat', "518, 26.0, 65, 7.0"),
+  createData('Lollipop', "392, 0.2, 98, 0.0"),
+  createData('Marshmallow', "318, 0, 81, 2.0"),
+  createData('Nougat', "360, 19.0, 9, 37.0"),
+  createData('Oreo', "437, 18.0, 63, 4.0"),
+  
+];
+
+function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+type Order = 'asc' | 'desc';
+
+function getComparator<Key extends keyof any>(
+  order: Order,
+  orderBy: Key,
+): (
+  a: { [key in Key]: number | string },
+  b: { [key in Key]: number | string },
+) => number {
+  return order === 'desc'
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+// This method is created for cross-browser compatibility, if you don't
+// need to support IE11, you can use Array.prototype.sort() directly
+function stableSort<T>(array: readonly T[], comparator: (a: T, b: T) => number) {
+  const stabilizedThis = array.map((el, index) => [el, index] as [T, number]);
+  stabilizedThis.sort((a, b) => {
+    const order = comparator(a[0], b[0]);
+    if (order !== 0) {
+      return order;
+    }
+    return a[1] - b[1];
+  });
+  return stabilizedThis.map((el) => el[0]);
+}
+
+interface HeadCell {
+  id: keyof Data;
+  label: string;
+  numeric: boolean;
+}
+
+const headCells: readonly HeadCell[] = [
+  {
+    id: 'name',
+    numeric: false,
+    label: 'Name',
+  },
+  {
+    id: 'link',
+    numeric: false,
+    label: 'Profile link',
+  },
+];
+
+interface EnhancedTableProps {
+  onRequestSort: (event: React.MouseEvent<unknown>, property: keyof Data) => void;
+  order: Order;
+  orderBy: string;
+}
+
+function EnhancedTableHead(props: EnhancedTableProps) {
+  const { order, orderBy, onRequestSort } =
+    props;
+  const createSortHandler =
+    (property: keyof Data) => (event: React.MouseEvent<unknown>) => {
+      onRequestSort(event, property);
+    };
+
+  return (
+    <TableHead>
+      <TableRow>
+        {headCells.map((headCell) => (
+          <TableCell
+            key={headCell.id}
+            align='left'
+            // padding='normal'
+            border-spacing='30px'
+            sortDirection={orderBy === headCell.id ? order : false}
+          >
+            <TableSortLabel
+              active={orderBy === headCell.id}
+              direction={orderBy === headCell.id ? order : 'asc'}
+              onClick={createSortHandler(headCell.id)}
+            >
+              {headCell.label}
+              {orderBy === headCell.id ? (
+                <Box component="span" sx={visuallyHidden}>
+                  {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                </Box>
+              ) : null}
+            </TableSortLabel>
+          </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
+  );
+}
+
+
+export default function EnhancedTable() {
+  const [order, setOrder] = React.useState<Order>('asc');
+  const [orderBy, setOrderBy] = React.useState<keyof Data>('name');
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+  const handleRequestSort = (
+    event: React.MouseEvent<unknown>,
+    property: keyof Data,
+  ) => {
+    const isAsc = orderBy === property && order === 'asc';
+    setOrder(isAsc ? 'desc' : 'asc');
+    setOrderBy(property);
+  };
+
+  const handleChangePage = (event: unknown, newPage: number) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  // Avoid a layout jump when reaching the last page with empty rows.
+  const emptyRows =
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
+
+  return (
+    <Box sx={{ width: '80%' }}>
+      <Paper sx={{ width: '100%', mb: 2 }}>
+        <TableContainer>
+          <Table>
+            <EnhancedTableHead
+              order={order}
+              orderBy={orderBy}
+              onRequestSort={handleRequestSort}
+            />
+            <TableBody>
+              {stableSort(rows, getComparator(order, orderBy))
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map((row, index) => {
+                  return (
+                    <TableRow
+                      hover
+                      role="checkbox"
+                      tabIndex={-1}
+                      key={row.name}
+                    >
+                      <TableCell style = {{ width:'50%'}} align="left">{row.name}</TableCell>
+                      <TableCell align="left">{row.link}</TableCell>
+                    </TableRow>
+                  );
+                })}
+              {emptyRows > 0 && (
+                <TableRow
+                  style={{
+                    height: 53 * emptyRows,
+                  }}
+                >
+                  <TableCell colSpan={2} />
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[5, 7, 10]}
+          component="div"
+          count={rows.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+      </Paper>
+    </Box>
+  );
+}
--- spa/src/pages/SearchJobs.tsx
+++ spa/src/pages/SearchJobs.tsx
@@ -1,12 +1,89 @@
 import * as React from "react";
+import { TagsInput } from "react-tag-input-component";
+import { useNavigate } from "react-router-dom";
+import FancyTableJobs from "../components/FancyTableJobs";
+import "../csss/SearchJobs.css";
+import logo from '../img/logo_softlink.png';
 
 interface Props {
 }
 
 const SearchJobs: React.FC<Props> = () => {
+    const navigate = useNavigate();
+    const redirectHome = () => {
+        let path= '/';
+        navigate(path);
+    }
+
+    const jobTitles = [
+        "Software engineer",
+        "DevOps",
+        "Software developer",
+        "Site reliability engineer",
+        "Business analyst",
+    ];
+
+    const [jobTitle, setJobTitle] = React.useState("");
+    let lst:string[] = [];
+    const [skills, setSkills] = React.useState(lst);
+    const [location, setLocation] = React.useState("");
+    const [r, setR] = React.useState(false);
+
+    const searchJobs = () => {
+        // Request la API
+        setR(true);
+    }
+
+    const onChangeJobTitle = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setJobTitle(event.target.value);
+    };
+    const onChangeLocation = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setLocation(event.target.value);
+    };
+
+
     return (
-        <div>
-            This is SearchJobs.
+        <div >
+            <div className="container cntr">
+                <img className="logo" src={logo} alt="Logo" onClick={redirectHome} />
+            </div>
+            <div className="divStyle">
+                <div className="margin_box">
+                    <div className="titles">
+                        Job title:
+                    </div>
+                    <div className="go309598777">
+                        <input className="go3450369076" type="tex" onChange={onChangeJobTitle} placeholder="Enter job title" />
+                    </div>
+                </div>
+                <br />
+                <div className="margin_box skills_box">
+                    <div className="titles">
+                        Skills:
+                    </div>
+                    <TagsInput
+                        value={skills}
+                        onChange={setSkills}
+                        name="Skills"
+                        placeHolder="Type and press enter"
+                    />
+                </div>
+                <br />
+                <div className="margin_box">
+                    <div className="titles">
+                        Location:
+                    </div>
+                    <div className="go309598777">
+                        <input className="go3450369076" type="tex" onChange={onChangeLocation} placeholder="Enter location" />
+                    </div>
+                </div>
+                <button className="button_img" onClick={searchJobs}>Search</button>
+            </div>
+            { r &&
+                <div className="divStyleTable">
+                    <FancyTableJobs></FancyTableJobs>
+                </div>
+            }
         </div>
     );
 }
